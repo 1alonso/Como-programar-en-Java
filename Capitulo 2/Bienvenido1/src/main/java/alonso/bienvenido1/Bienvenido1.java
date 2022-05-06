be0ff940ac7b844b659ded36eaebc1258f89e1ec
1alonso <89498371+1alonso@users.noreply.github.com>
2022-05-06 08:36:32 -0700

    Add files via upload

---
+++ Capitulo 2/Bienvenido1/src/main/java/alonso/bienvenido1/Bienvenido1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package alonso.bienvenido1;
+
+// Fig. 2.1: Bienvenido1.java
+// Programa para imprimir texto
+
+public class Bienvenido1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }// fin del metodo main
+}// fin de la case Bienvendo1
