d20ee5cead0fcdae2f5981d8e20bbc56ce5e9e28
1alonso <89498371+1alonso@users.noreply.github.com>
2022-05-06 08:37:54 -0700

    Add files via upload

---
+++ Capitulo 2/Bienvenido3/src/main/java/alonso/bienvenido3/Bienvenido3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package alonso.bienvenido3;
+
+// Fig. 2.4: Bienvenido3.java
+// Imprimir varias lineas de texto con una sola instruccion.
+
+public class Bienvenido3 {
+
+    // el metodo main empieza la ejecucion de la aplicaion en java
+    public static void main(String[] args) {
+        System.out.println("Bienvenido\na\nla programacion\nen Java!");
+    }
+}
