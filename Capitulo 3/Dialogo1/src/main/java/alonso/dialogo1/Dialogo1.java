c7b864888980a4e8b90d9bc531fcf55a5c89fca7
1alonso <89498371+1alonso@users.noreply.github.com>
2022-05-06 15:46:09 -0700

    Add files via upload

---
+++ Capitulo 3/Dialogo1/src/main/java/alonso/dialogo1/Dialogo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package alonso.dialogo1;
+
+// Fig. 3.12: Dialogo1.java
+// Uso de JOptionPane para mostrar varias lineas en un cuadro de dialogo.
+import javax.swing.JOptionPane;
+
+public class Dialogo1 {
+
+    public static void main(String[] args) {
+        // muestra un dialogo con un mensaje
+        JOptionPane.showMessageDialog(null, "Bienvenido a Java");
+    }
+}
