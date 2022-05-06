c9d51edb1aa8176104ae5920c9afb1943d47194d
1alonso <89498371+1alonso@users.noreply.github.com>
2022-05-06 08:37:13 -0700

    Add files via upload

---
+++ Capitulo 2/Bienvenido2/src/main/java/alonso/bienvenido2/Bienvenido2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package alonso.bienvenido2;
+
+// Fig. 2.3: Bienvenido2.java
+// Imprimir una linea de texto con varias instrucciones.
+public class Bienvenido2 {
+
+    // el metodo main empieza la ejecucion de la aplicacion en Java
+    public static void main(String[] args) {
+        System.out.print("Bienvenido a ");
+        System.out.println("la programacion en Java!");
+    } // fin del metodo main
+} // fin de la clase Bienvenido2
